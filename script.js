05abb2e5c6634a4250160f15e5372551b7a397c0
YCYLSTUDIO <157963447+YCYLSTUDIO@users.noreply.github.com>
2024-10-27 17:58:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -555,3 +555,12 @@ function sjjtfunc(num, bool) {
     }
   }
 }
+
+function updateScreenSize() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  alert(`Lebar layar: ${screenWidth}px, Tinggi layar: ${screenHeight}px`);
+}
+
+updateScreenSize();
+window.addEventListener("resize", updateScreenSize);
